1050f3f373811a2347d5d710214b9e1865975b04
Minsik <mscho527@gmail.com>
2021-07-28 11:05:36 -0500

    Add threshold for max distance between MLWF & atom center

--- pdmet/tools/misc.py
+++ pdmet/tools/misc.py
@@ -25,16 +25,17 @@ import numpy as np
             
             
 BOHR = 0.52917721092
-def make_imp_orbs(cell, w90, impCluster, threshold=0.5, rm_list=None, add_list=None):
+def make_imp_orbs(cell, w90, impCluster, threshold=1.0, rm_list=None, add_list=None):
     '''Attribute:
             cell            : PySCF cell object
             w90             : the w90 object for MLWFs
             impCluster      : a list of the atom labels starting from 1
        Return:
             impOrbs         : list of the MWLFs that belong to the impCluster
-
-    THIS IS A MODIFIED VERSION THAT DOES NOT USE THRESHOLDING BASED ON THE DISTANCE
-
+       
+       ** Note: Unlike the original pDMET code, MLWFs are assigned to atoms beforehand
+                regardless of the chosen impurity atoms
+       ** Not implemented: rm_list & add_list
     '''
     impCluster = np.asarray(impCluster)
 
@@ -61,7 +62,6 @@ def make_imp_orbs(cell, w90, impCluster, threshold=0.5, rm_list=None, add_list=N
     MLWFs_coors = put_atoms_in_unitcell(MLWFs_frac_coors) @ lattice
 
     # Modified code starts here
-
     # Calculate the distance between MLWFs and the all atoms
     tmp = np.repeat(MLWFs_coors[:,np.newaxis,:], abs_coors.shape[0], axis=1)
     distance = np.sqrt(np.sum((tmp - abs_coors)**2, axis=2))
@@ -71,12 +71,12 @@ def make_imp_orbs(cell, w90, impCluster, threshold=0.5, rm_list=None, add_list=N
     impOrbs = np.zeros(num_wann, dtype=int)
 
     for atom in impCluster:
-        impOrbs += (min_distance_idx == (atom - 1))
+        impOrbs += np.logical_and(min_distance_idx == (atom - 1), min_distance < threshold)
 
     # Group the impurity orbitals by their corresponding atoms
     Norbs = MLWFs_coors.shape[0]
     impAtms = []
     for i in impCluster:
-        impAtms.append(np.arange(Norbs)[min_distance_idx == (i-1)])
+        impAtms.append(np.arange(Norbs)[np.logical_and(min_distance_idx == (i-1), min_distance < threshold)])
 
     return impOrbs, impAtms
